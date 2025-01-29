f477788dd7d6dbb8c9a30d848c5b30194b01ffe0
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-29 20:23:26 +0530

    source common

--- 05-if.sh
+++ 05-if.sh
@@ -2,16 +2,17 @@ fruit_name=$1
 quantity=$2
 
 if [ -z "$fruit_name" ]; then
-echo Input Missing
-
+  echo Input Missing
 fi
+
 if [ "$fruit_name" == "mango" ]
 then
-echo Mango Quantity $quantity
+  echo Mango Quantity $quantity
 I
 else
-echo Fruit does not exist
+  echo Fruit does not exist
 fi
+
 ## Always double quote variables in expressions
 
 if [ "$quantity" -gt 100 ]; then
