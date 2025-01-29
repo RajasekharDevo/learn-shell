840cb936a929a0370d7523a45c36859def9ea646
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-29 20:19:35 +0530

    source common

--- 05-if.sh
+++ 05-if.sh
@@ -5,7 +5,7 @@ if [ -z "$fruit_name" ]; then
 echo Input Missing
 
 fi
-if [ "$fruit_name" == "mango"]
+if [ "$fruit_name" == "mango" ]
 then
 echo Mango Quantity $quantity
 I
@@ -13,7 +13,7 @@ else
 echo Fruit does not exist
 fi
 ## Always double quote variables in expressions
-if [ "$quantity" -gt 100]; then
+if [ "$quantity" -gt 100 ]; then
 echo Mango Price 0.5$
 else
 echo Mango Price 1$
