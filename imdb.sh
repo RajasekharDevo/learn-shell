7d8dc2f0874bc51184a5fe1493edf50e9809dcb3
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-30 12:39:29 +0530

    source common

--- imdb.sh
+++ imdb.sh
@@ -9,7 +9,7 @@ exit
 fi
 
 percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F
-'{print $2}')
+ = '{print $2}' | awk -F . '{print $1}')
 
 echo Percent $percent
 if  [ "$percent" -ge 70 ]; then
