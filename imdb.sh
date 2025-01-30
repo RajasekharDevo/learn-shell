88fd1813d4352c49685d6216f8461c2fec8084e7
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-30 12:15:03 +0530

    source common

--- imdb.sh
+++ imdb.sh
@@ -1,13 +1,14 @@
 # Get rating
 # > 7 - Good
-# > 7 - Average
+# < 7 - Average
 
 input=$1
 if [ -z "$input" ]; then
 echo Input Movie Name Missing
 exit
 fi
-percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F
+
+percent=$(curl -s https://www.themoviedb.org/movie/640146-ant-man-and-the-wasp-quantumania | grep data-percent | awk -f
 = '{print $2}')
 
 echo Percent $percent
