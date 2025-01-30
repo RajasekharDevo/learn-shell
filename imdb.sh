02c6e4ed45386359e10399be5c1ce53bc23caaec
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-30 12:21:08 +0530

    source common

--- imdb.sh
+++ imdb.sh
@@ -8,8 +8,8 @@ echo Input Movie Name Missing
 exit
 fi
 
-percent=$(curl -s https://www.themoviedb.org/movie/640146-ant-man-and-the-wasp-quantumania | grep data-percent | awk -f
-= '{print $2}')
+percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F
+'{print $2}')
 
 echo Percent $percent
 if  [ "$percent" -ge 70 ]; then
