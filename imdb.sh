db4b59cd0cf5211c6324c9f04b9d45b317089fcf
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-30 12:44:37 +0530

    source common

--- imdb.sh
+++ imdb.sh
@@ -4,14 +4,15 @@
 
 input=$1
 if [ -z "$input" ]; then
-echo Input Movie Name Missing
-exit
+  echo Input Movie Name Missing
+  exit
 fi
 
 percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F
  = '{print $2}' | awk -F . '{print $1}')
 
 echo Percent $percent
+
 if  [ "$percent" -ge 70 ]; then
   echo Good
 else
